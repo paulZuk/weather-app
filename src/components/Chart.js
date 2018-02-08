429a6d34e27d0b2c973c1bd6daf90d1dce2e39c9
paulZuk <k4mpain@gmail.com>
2018-02-08 22:34:55 +0100

    added chart component

---
+++ src/components/Chart.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+
+export default (props) => {
+    return (
+        <div>
+            <Sparklines data={props.data}>
+                <SparklinesLine color={props.color} />
+                <SparklinesReferenceLine type="avg" />
+            </Sparklines>
+        </div>
+    )
+}
--- src/containers/WheaterList.js
+++ src/containers/WheaterList.js
@@ -1,26 +1,43 @@
 import React , { Component } from 'react';
+import _ from 'lodash';
 import { connect } from 'react-redux';
+import Chart from '../components/Chart';
 
 class WheaterList extends Component {
     renderWeather(cityData) {
 
         const name = cityData.city.name;
+        const temp = _.map(cityData.list.map(elem => elem.main.temp), temp => temp - 272.15);
+        const pressure = cityData.list.map(elem => elem.main.pressure);
+        const humidity = cityData.list.map(elem => elem.main.humidity);
+
+        let avgTemp = _.round(_.sum(temp)/temp.length,0);
+
+        console.log(humidity);
 
         return (
             <tr key={name}>
                 <td>{name}</td>
+                <td>
+                    <Chart color="red" data={temp} />
+                    <div>{`${avgTemp} \u{2103}`}</div>
+                </td>
+                <td><Chart color="blue" data={pressure} /></td>
+                <td><Chart color="green" data={humidity} /></td>
             </tr>
         )
     }
     render() {
+        console.log(this.props.weather);
+        
         return (
             <table className="table table-hover">
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature (&#8451;)</th>
+                        <th>Pressure (hPa)</th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody>
